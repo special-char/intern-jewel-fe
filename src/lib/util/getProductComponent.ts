30820a1348d4232e88189d7c7a1965e1d1477c87
kalpesh-vala <22bce375@nirmauni.ac.in>
2025-06-09 14:57:54 +0530

    feature : product card design-3 added for homelayout-3

--- src/lib/util/getProductComponent.ts
+++ src/lib/util/getProductComponent.ts
@@ -1,5 +1,6 @@
 import Design1 from "@/blocks/ProductsCollection/components/productcard-design/Design1"
 import Design2 from "@/blocks/ProductsCollection/components/productcard-design/Design2"
+import Design3 from "@/blocks/ProductsCollection/components/productcard-design/Design3"
 
 const getProductComponent = (designType: string) => {
   switch (designType) {
@@ -7,6 +8,8 @@ const getProductComponent = (designType: string) => {
       return Design1
     case "design2":
       return Design2
+    case "design3":
+      return Design3
     default:
       return Design1
   }
