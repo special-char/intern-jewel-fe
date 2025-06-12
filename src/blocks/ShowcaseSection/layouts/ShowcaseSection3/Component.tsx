96db8bb33ef36652f2a05e3aa93c9a542ea9a3d1
ankitpandor <pandorankit3199@gmail.com>
2025-06-12 17:44:10 +0530

    showcaseSection bg image done

--- src/blocks/ShowcaseSection/layouts/ShowcaseSection3/Component.tsx
+++ src/blocks/ShowcaseSection/layouts/ShowcaseSection3/Component.tsx
@@ -5,21 +5,12 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import React from "react"
 
 export const ShowcaseSection3 = (props: ShowcaseLayout3) => {
-  const {
-    image,
-    showcaseHeading,
-    showcaseDescription,
-    button,
-  } = props
+  const { image, showcaseHeading, showcaseDescription, button } = props
 
   return (
     <section className="relative w-full h-[90vh] text-white overflow-hidden">
       {/* Background Image */}
-      <ImageMedia
-        resource={image as Media}
-        imgClassName="object-cover"
-        fill
-      />
+      <ImageMedia resource={image as Media} imgClassName="object-cover" fill />
 
       {/* Base overlay for mild dark tint */}
       <div className="absolute inset-0 bg-black/10 z-10" />
@@ -28,14 +19,17 @@ export const ShowcaseSection3 = (props: ShowcaseLayout3) => {
       <div className="block lg:hidden absolute inset-0 z-20 bg-gradient-to-b from-transparent via-black/40 to-black" />
 
       {/* Text Content */}
-      <div className="absolute inset-0 z-30 flex flex-col items-center justify-center text-center px-4 md:px-10">
+      <div className="font-display-large absolute inset-0 z-30 flex flex-col items-center justify-center text-center px-4 md:px-10">
+        <div
+          className="w-12 h-12 md:w-16 md:h-16 mb-6 bg-no-repeat bg-contain bg-center opacity-80 filter invert"
+          style={{ backgroundImage: "url('/images/test.svg')" }}
+        />
+
         {showcaseHeading && (
-          <h1 className="text-10xl md:text-6xl font-serif mb-4">
-            {showcaseHeading}
-          </h1>
+          <h1 className="font-display-large mt-4 mb-4">{showcaseHeading}</h1>
         )}
         {showcaseDescription && (
-          <p className="max-w-xl text-base md:text-lg text-white/90 mb-6">
+          <p className="max-w-xl  text-base md:text-lg text-white/90 mb-6">
             {showcaseDescription}
           </p>
         )}
