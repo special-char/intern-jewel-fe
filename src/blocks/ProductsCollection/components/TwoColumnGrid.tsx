0736c113cf4c2307f32ff4efe04a96deab05d419
Manav2905 <manavkakkad2905@gmail.com>
2025-06-13 11:46:01 +0530

    finally done

--- src/blocks/ProductsCollection/components/TwoColumnGrid.tsx
+++ src/blocks/ProductsCollection/components/TwoColumnGrid.tsx
@@ -20,6 +20,7 @@ export const TwoColumnGrid = async ({
             ? await listProducts({
                 countryCode: DEFAULT_COUNTRYCODE,
                 queryParams: {
+                    //@ts-expect-error
                     id: limitedProducts.map((p) => p.product_id),
                     fields:
                         "*variants.calculated_price,*metadata,+variants.inventory_quantity , *images,+images.url , *thumbnail",
@@ -35,7 +36,7 @@ export const TwoColumnGrid = async ({
 
 
     return (
-<div className="grid grid-cols-2 gap-4 lg:gap-6 h-full">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-10 gap-y-14 bg-neutral-50 p-8">
                 {pricedProducts &&
                     pricedProducts?.map((item) => {
                         return (
