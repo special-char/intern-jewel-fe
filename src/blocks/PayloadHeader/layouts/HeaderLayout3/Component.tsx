a621f18e31900685edf21533b04116c2724b0b11
jaydip <jaydipbsatani@gmail.com>
2025-06-16 10:59:04 +0530

    static data fix

--- src/blocks/PayloadHeader/layouts/HeaderLayout3/Component.tsx
+++ src/blocks/PayloadHeader/layouts/HeaderLayout3/Component.tsx
@@ -50,7 +50,7 @@ export default async function HeaderLayout3(props: firstLayout) {
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.35 2.7a1 1 0 00.9 1.3h12.1M16 17a1 1 0 11-2 0 1 1 0 012 0zm-6 0a1 1 0 11-2 0 1 1 0 012 0z"
                   />
                 </svg>
-                <span className="absolute -top-2 -right-2 bg-primary text-black text-xs w-4 h-4 flex items-center justify-center rounded-full">
+                <span className="absolute -top-2 -right-2 bg-primary text-background text-xs w-4 h-4 flex items-center justify-center rounded-full">
                   0
                 </span>
               </LocalizedClientLink>
