48badba4ca4d691eb937e35a3b171811ff083213
Dhruvesh <vaghasiyadhruvesh@gmail.com>
2025-06-16 11:40:30 +0530

    Changes in static data

--- src/blocks/CategoryShowcase/layouts/CategoryLayout2/Component.tsx
+++ src/blocks/CategoryShowcase/layouts/CategoryLayout2/Component.tsx
@@ -7,7 +7,7 @@ export function CategoryLayout2(props: CategoryLayoutType) {
     const { categories } = props
 
     return (
-        <section className="p-0 mt-20 mb-20">
+        <section className="content-container mt-20 mb-20">
             <div className="flex flex-wrap justify-center gap-x-7 gap-y-10">
                 {categories?.map((category) => (
                     <div
@@ -26,15 +26,14 @@ export function CategoryLayout2(props: CategoryLayoutType) {
                             />
 
                             {/* Hover Image with Text */}
-                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out border border-[#b59a6c]">
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out border border-primary">
                                 <ImageMedia
                                     resource={category.image as Media}
-                                    imgClassName="w-[85%] h-[85%] object-contain [transform:rotateZ(25deg)] group-hover:rotate-0 transition-transform duration-500 ease-in-out"
+                                    imgClassName="w-[85%] h-[85%] object-contain rotate-[25deg] group-hover:rotate-0 transition-transform duration-500 ease-in-out"
                                     fill
                                 />
                                 <div
-                                    className="absolute bottom-4 left-0 right-0 bg-inherit bg-opacity-80 text-[color:var(--muted-foreground)] text-center text-base py-2 px-3"
-                                    style={{ letterSpacing: "7px" }}
+                                    className="absolute bottom-4 left-0 right-0 bg-inherit bg-opacity-80 text-spacing text-center text-muted-foreground py-2 px-3"
                                 >
                                     {category.text}
                                 </div>
