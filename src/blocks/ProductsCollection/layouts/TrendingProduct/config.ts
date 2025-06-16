98f7ff9202a98fda60c8caf5890fff12e2098d9f
Manav2905 <manavkakkad2905@gmail.com>
2025-06-16 15:06:04 +0530

    done after adding default value

--- src/blocks/ProductsCollection/layouts/TrendingProduct/config.ts
+++ src/blocks/ProductsCollection/layouts/TrendingProduct/config.ts
@@ -1,3 +1,8 @@
+import {
+  FixedToolbarFeature,
+  InlineToolbarFeature,
+  lexicalEditor,
+} from "@payloadcms/richtext-lexical"
 import { Block } from "payload"
 import { button } from "@/fields/button"
 
@@ -17,6 +22,42 @@ export const TrendingProduct: Block = {
       name: "subtext",
       type: "richText",
       required: true,
+      editor: lexicalEditor({
+        features: ({ rootFeatures }) => {
+          return [
+            ...rootFeatures,
+            FixedToolbarFeature(),
+            InlineToolbarFeature(),
+          ];
+        },
+      }),
+      defaultValue: {
+        root: {
+          type: "root",
+          format: "",
+          indent: 0,
+          version: 1,
+          children: [
+            {
+              type: "paragraph",
+              format: "",
+              indent: 0,
+              version: 1,
+              children: [
+                {
+                  type: "text",
+                  detail: 0,
+                  format: 0,
+                  mode: "normal",
+                  style: "",
+                  text: "Discover our handpicked selection of exquisite jewelry pieces, curated to elevate your style and capture the essence of sophistication.",
+                  version: 1
+                },
+              ],
+            },
+          ],
+        },
+      },
     },
     {
       name: "products",
@@ -24,6 +65,13 @@ export const TrendingProduct: Block = {
       relationTo: "products",
       hasMany: true,
       required: true,
+      defaultValue: async ({ req }) => {
+        const products = await req.payload.find({
+          collection: "products",
+          limit: 2,
+        })
+        return products.docs.map((product) => product.id)
+      },
       validate: (val) => {
         if (!val || val.length !== 2) {
           return "You must select exactly two products."
