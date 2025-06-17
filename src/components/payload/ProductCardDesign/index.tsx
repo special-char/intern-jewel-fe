8e1d3fa91a1e7359d2bd154f3a73b3c9156bbdc7
kalpesh-vala <22bce375@nirmauni.ac.in>
2025-06-17 11:50:14 +0530

    tailwind fixes

--- src/components/payload/ProductCardDesign/index.tsx
+++ src/components/payload/ProductCardDesign/index.tsx
@@ -7,19 +7,19 @@ const options = [
     label: "Design 1",
     value: "design1",
     image:
-      "https://minio.thespecialcharacter.com/payload-website-templates/thumbnail-bannerlayout1.png",
+      "https://minio-usw040kwsgkookko0kswwkko.194.164.151.221.sslip.io/payload-jewel/product_card_design1.webp",
   },
   {
     label: "Design 2",
     value: "design2",
     image:
-      "https://minio.thespecialcharacter.com/payload-website-templates/thumbnail-bannerlayout1.png",
+      "https://minio-usw040kwsgkookko0kswwkko.194.164.151.221.sslip.io/payload-jewel/product_card_design2.webp",
   },
   {
     label: "Design 3",
     value: "design3",
     image:
-      "https://minio.thespecialcharacter.com/payload-website-templates/thumbnail-bannerlayout1.png",
+      "https://minio-usw040kwsgkookko0kswwkko.194.164.151.221.sslip.io/payload-jewel/product_card_design3.webp",
   },
 ]
 
@@ -32,20 +32,15 @@ export const CardDesignSelector: React.FC<any> = ({ path }) => {
         <div
           key={opt.value}
           onClick={() => setValue(opt.value)}
+          className="flex-1 p-2 rounded-base cursor-pointer text-center"
           style={{
-            border:
-              value === opt.value ? "2px solid #0073e6" : "1px solid #ccc",
-            borderRadius: 8,
-            padding: 8,
-            cursor: "pointer",
-            textAlign: "center",
-            width: 120,
+            border: value === opt.value ? "2px solid #0073e6" : "1px solid #ccc"
           }}
         >
           <img
             src={opt.image}
             alt={opt.label}
-            style={{ width: "100%", borderRadius: 4, marginBottom: 8 }}
+            className="w-full h-auto aspect-[9/16] rounded-soft mb-2 object-cover"
           />
           <div>{opt.label}</div>
         </div>
