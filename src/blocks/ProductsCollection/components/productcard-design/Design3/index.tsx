fa901d440a626130496ad348884d67eb30e05ace
kalpesh-vala <22bce375@nirmauni.ac.in>
2025-06-20 15:07:50 +0530

    design3 for product card updated

--- src/blocks/ProductsCollection/components/productcard-design/Design3/index.tsx
+++ src/blocks/ProductsCollection/components/productcard-design/Design3/index.tsx
@@ -11,17 +11,15 @@ const Design3 = ({
   variant: HttpTypes.StoreProductVariant
 }) => (
   <div
-    className="w-full aspect-3/4 flex flex-col gap-4 group ease-in-out hover:cursor-pointer relative overflow-hidden"
+    className="w-full aspect-3/4 grid grid-rows-[auto_1fr] gap-4 group ease-in-out hover:cursor-pointer relative overflow-hidden"
   >
     <div className="relative w-full aspect-square bg-card overflow-hidden flex items-center justify-center">
-      <div className="relative w-4/5 h-4/5 max-w-full max-h-full">
-        <ImageMedia
-          src={product?.thumbnail || product?.images?.[0]?.url || ("" as any)}
-          alt={product.title}
-          fill
-          imgClassName="object-contain group-hover:scale-110 duration-300 ease-in-out"
-        />
-      </div>
+      <ImageMedia
+        src={product?.thumbnail || product?.images?.[0]?.url || ("" as any)}
+        alt={product.title}
+        fill
+        imgClassName="object-contain group-hover:scale-110 duration-300 ease-in-out"
+      />
       <Button
         variant="design3"
         className="absolute left-0 bottom-0 w-full translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300 ease-in-out z-10 rounded-none"
@@ -29,8 +27,8 @@ const Design3 = ({
         Add to Cart
       </Button>
     </div>
-    <div className="flex flex-col justify-center px-2 pb-2 md:pb-0">
-      <h3 className="text-body font-dmsans font-bold line-clamp-2 group-hover:text-primary text-center text-sm sm:text-base">
+    <div className="flex flex-col">
+      <h3 className="text-body font-dmsans font-bold line-clamp-2 flex-1 group-hover:text-primary text-center">
         {product.title}
       </h3>
       <ProductPrice
