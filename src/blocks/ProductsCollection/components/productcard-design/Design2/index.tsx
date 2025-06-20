7ec7ed31afd9b7f09fbf966b8f8c0976b5667ebf
kalpesh-vala <22bce375@nirmauni.ac.in>
2025-06-20 16:04:41 +0530

    design 2 and design 3 updated with samller image size

--- src/blocks/ProductsCollection/components/productcard-design/Design2/index.tsx
+++ src/blocks/ProductsCollection/components/productcard-design/Design2/index.tsx
@@ -1,8 +1,9 @@
 import ProductPrice from "@modules/products/components/product-price"
 import { HttpTypes } from "@medusajs/types"
+import { Button } from "@lib/components/ui/button"
 import { ImageMedia } from "@components/payload/Media/ImageMedia"
 
-const Design2 = ({
+const Design3 = ({
   product,
   variant,
 }: {
@@ -10,23 +11,20 @@ const Design2 = ({
   variant: HttpTypes.StoreProductVariant
 }) => (
   <div
-    style={{
-      display: "grid",
-      gridTemplate: "subgrid",
-      gridTemplateRows: "1fr minmax(70px, auto) auto",
-    }}
-    className="w-full aspect-3/4 gap-4 group ease-in-out hover:cursor-pointer"
+    className="w-full aspect-3/4 grid grid-rows-[auto_1fr] gap-4 group ease-in-out hover:cursor-pointer relative overflow-hidden"
   >
-    <div className="relative w-full aspect-square bg-card">
-      <ImageMedia
-        src={product?.thumbnail || product?.images?.[0]?.url || ("" as any)}
-        alt={product.title}
-        fill
-        imgClassName="object-contain group-hover:scale-110 duration-300 ease-in-out p-4 "
-      />
+    <div className="relative w-full aspect-square bg-card overflow-hidden flex items-center justify-center p-4">
+      <div className="relative w-2/3 h-2/3">
+        <ImageMedia
+          src={product?.thumbnail || product?.images?.[0]?.url || ("" as any)}
+          alt={product.title}
+          fill
+          imgClassName="object-contain group-hover:scale-110 duration-300 ease-in-out"
+        />
+      </div>
     </div>
     <div className="flex flex-col">
-      <h3 className="text-body font-dmsans font-bold line-clamp-2 flex-1 group-hover:text-primary text-center">
+      <h3 className="text-body font-dmsans font-bold line-clamp-2 group-hover:text-primary text-center">
         {product.title}
       </h3>
       <ProductPrice
@@ -35,8 +33,7 @@ const Design2 = ({
         className="text-center"
       />
     </div>
-    {/* <Button>Add to Cart</Button> */}
   </div>
 )
 
-export default Design2
+export default Design3
--- src/blocks/ProductsCollection/components/productcard-design/Design3/index.tsx
+++ src/blocks/ProductsCollection/components/productcard-design/Design3/index.tsx
@@ -13,13 +13,15 @@ const Design3 = ({
   <div
     className="w-full aspect-3/4 grid grid-rows-[auto_1fr] gap-4 group ease-in-out hover:cursor-pointer relative overflow-hidden"
   >
-    <div className="relative w-full aspect-square bg-card overflow-hidden flex items-center justify-center">
-      <ImageMedia
-        src={product?.thumbnail || product?.images?.[0]?.url || ("" as any)}
-        alt={product.title}
-        fill
-        imgClassName="object-contain group-hover:scale-110 duration-300 ease-in-out"
-      />
+    <div className="relative w-full aspect-square bg-card overflow-hidden flex items-center justify-center p-4">
+      <div className="relative w-2/3 h-2/3">
+        <ImageMedia
+          src={product?.thumbnail || product?.images?.[0]?.url || ("" as any)}
+          alt={product.title}
+          fill
+          imgClassName="object-contain group-hover:scale-110 duration-300 ease-in-out"
+        />
+      </div>
       <Button
         variant="design3"
         className="absolute left-0 bottom-0 w-full translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300 ease-in-out z-10 rounded-none"
@@ -28,7 +30,7 @@ const Design3 = ({
       </Button>
     </div>
     <div className="flex flex-col">
-      <h3 className="text-body font-dmsans font-bold line-clamp-2 flex-1 group-hover:text-primary text-center">
+      <h3 className="text-body font-dmsans font-bold line-clamp-2 group-hover:text-primary text-center">
         {product.title}
       </h3>
       <ProductPrice
